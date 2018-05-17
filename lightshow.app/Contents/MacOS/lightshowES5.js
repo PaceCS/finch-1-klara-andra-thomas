dbd10d489141f1b30028a85c207681234e7191a6
klara-andra-thomas <klara.andrathomas20@paceacademy.org>
2018-05-17 09:16:31 -0400

    Done

--- lightshow.app/Contents/MacOS/lightshowES5.js
+++ lightshow.app/Contents/MacOS/lightshowES5.js
@@ -13,20 +13,35 @@ var finch = new Finch();
 finch.setLED(255, 0, 0);
 wait(1000);
 finch.setLED(0, 255, 0);
+wait(1000);
 
 // Traffic Light
 // Write your code here to model 3 cycles of a traffic light
-
+for (var number = 1; number <= 3; number += 1) {
+    finch.setLED(255, 0, 0);
+    wait(3000);
+    finch.setLED(255, 255, 0);
+    wait(3000);
+    finch.setLED(0, 255, 0);
+    wait(3000);
+}
 
 // Light show
 // Write your own code here to create a light show of your choosing
 
 
-function wait(secs, buffer) {
-    var buf = buffer ? buffer : .1;
+finch.setLED(255, 143, 143);
+wait(3000);
+finch.setLED(143, 255, 143);
+wait(3000);
+finch.setLED(143, 143, 255);
+wait(3000);
+
+function wait(msecs, buffer) {
+    var buf = buffer ? buffer : 100;
     var time1 = new Date().getTime();
     var time2 = new Date().getTime();
-    while (time2 < time1 + secs + buf) {
+    while (time2 < time1 + msecs + buf) {
         time2 = new Date().getTime();
     }
 }
--- lightshow.js
+++ lightshow.js
@@ -15,12 +15,25 @@ wait(1000);
 
 // Traffic Light
 // Write your code here to model 3 cycles of a traffic light
-
+for (let number = 1; number <= 3; number += 1){
+  finch.setLED(255,0,0);
+  wait(3000);
+  finch.setLED(255,255,0);
+  wait(3000);
+  finch.setLED(0,255,0);
+  wait(3000);
+}
 
 // Light show
 // Write your own code here to create a light show of your choosing
 
 
+finch.setLED(255,143,143);
+wait(3000);
+finch.setLED(143,255,143);
+wait(3000);
+finch.setLED(143,143,255);
+wait(3000);
 
 
 
